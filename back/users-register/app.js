5c9ade3e6ab8e1140454662ff1f097b98e5f5383
Tarsis de Lima <br.tmvdl@gmail.com>
2022-10-01 10:44:25 -0300

    Fixing '/users/register' endpoint

--- back/users-register/app.js
+++ back/users-register/app.js
@@ -1,11 +1,11 @@
 const { DuplicatedError } = require('/project/commons/errors')
 const userIndex = require('/project/commons/db').in('users')
 
-module.exports = ({ body: { email } }, res) => {
+module.exports = ({ body: { email, password } }, res) => {
   if (userIndex.find({ email }))
     throw new DuplicatedError({ email })
 
   const created_at = Date.now().toString()
-  userIndex.new().writeMany({ email, created_at })
+  userIndex.new().writeMany({ email, password, created_at })
   return res.json({ created_at })
 }
